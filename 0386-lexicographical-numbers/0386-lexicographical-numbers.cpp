e28964097c6acd71767e6cec6d98737a02834166
Ajay Kumar Soni <ajayksoni198@gmail.com>
2025-06-08 10:38:04 +0530

    Time: 0 ms (100%), Space: 13.9 MB (83.88%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> lexicographicalNumbers;
+        int currentNumber = 1;
+
+        // Generate numbers from 1 to n
+        for (int i = 0; i < n; ++i) {
+            lexicographicalNumbers.push_back(currentNumber);
+
+            if (currentNumber * 10 <= n) {
+                currentNumber *= 10;
+            } else {
+                // Adjust the current number by moving up one digit
+                while (currentNumber % 10 == 9 || currentNumber >= n) {
+                    currentNumber /= 10;  // Remove the last digit
+                }
+                currentNumber += 1;  // Increment the number
+            }
+        }
+
+        return lexicographicalNumbers;
+    }
+};
